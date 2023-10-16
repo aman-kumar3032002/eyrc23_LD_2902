afda553e2e78673a5f9c170cea7a8de593f5978e
Aman Kumar <kumaraman3032002@gmail.com>
2023-10-16 19:21:32 +0530

    written inside the position_hold.py

---
+++ new_waypoint_navigation.py
@@ -0,0 +1,13 @@
+
+'''
+Team ID: 2902
+Theme: Luminosity Drone
+Author List: Aman Kumar, Harsh gulzar, Manila Raj Putra, Shivam Kumar
+Filename: LD_2902_position_hold.py
+Functions: disarm(),arm(),whycon_callback(),altitude_set_pid(),pitch_set_pid(),roll_set_pid(),pid()
+Global variables: None
+'''
+#importing swift class from LD_2902_position_hold module
+from LD_2902_position_hold import swift
+from heelo import LD_2902_position_hold
+print("hello")

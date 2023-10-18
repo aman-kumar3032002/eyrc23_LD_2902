d6217b7591f064d6fba9d9628f0c85a81e61b698
ShivamMIT007 <143291911+ShivamMIT007@users.noreply.github.com>
2023-10-18 14:44:36 +0530

    Add files via upload

---
+++ led_detection#2902.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import numpy as np
+
+#read image
+img = cv.imread('openCV/sample.jpg')
+cv.imshow('Sample', img)
+
+#find no. of led
+imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(imgray, 225, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+print("Number of LEDs = " + str(len(contours)))
+
+#Drawing contours
+cv.drawContours(img, contours, -1, (0, 255, 0), 2, lineType= cv.LINE_AA)
+
+#Finding area & centroid
+led_info = []
+
+for contour in contours:
+    area = cv.contourArea(contour)
+
+    M = cv.moments(contour)
+    if M["m00"]!= 0:
+            cx = float(M["m10"]/M["m00"])
+            cy = float(M["m01"]/M["m00"])
+            led_info.append({'centroid': (cx,cy), 'area:': area })
+
+for i, led in enumerate(led_info):
+    print(f"LED {i+1}: Centroid {led['centroid']}")
+    print(f"Contour Area: {area}")
+
+#show no.of led 
+x,y,w,h = cv.boundingRect(contour)
+cv.putText(img, "+"+ str(len(contours)),(x-10,y-10),cv.FONT_HERSHEY_COMPLEX, .5, (0,255,0), 2)
+cv.imshow('Put Text', img)
+          
+cv.waitKey(0)
+cv.destroyAllWindows()
